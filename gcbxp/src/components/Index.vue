71417bf08009cc65ae1a7a417b459135daf6b6bc
murlokito <murlux@protonmail.com>
2019-05-27 02:42:04 +0100

    Added Node info to web app

--- gcbxp/src/components/Index.vue
+++ gcbxp/src/components/Index.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="w3-container w3-center">
     <h2 class="w3-margin">{{ indexMsg }}</h2>
+    <Node></Node>
   </div>
 </template>
 
 <script>
+
+import Node from './Node'
+
 export default {
   name: 'Index',
+  components: {
+    'Node': Node
+  },
   data () {
     return {
       indexMsg: 'Node Information'
---
+++ gcbxp/src/components/Node.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w3-container w3-center">
+      {{info}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Node',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    return fetch('/api/node_info', {
+      method: 'get' }).then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        this.info = data
+      }).catch((err) => console.log(err))
+  }
+}
+</script>
